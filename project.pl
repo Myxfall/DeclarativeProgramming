5f2e43a98017e346677fe09f990d51d9559e9a9d
Myxfall <maximilienromain96@hotmail.com>
2019-05-08 12:58:26 +0200

    [adding] buildind stack

--- project.pl
+++ project.pl
@@ -9,17 +9,17 @@
 % Teacher
 prof --> ["prof"].
 teacherName(Name) --> [Name], {string(Name)}.
-teacher --> prof, teacherName(Name).
-teacher --> ["he", "also"].
-teacher --> ["she", "also"].
+teacher(Name) --> prof, teacherName(Name).
+teacher(Name) --> ["he", "also"], {Name = "he"}.
+teacher(Name) --> ["she", "also"], {Name = "she"}.
 
 % Class
 classWord --> ["class"].
 classWord --> ["classes"].
 classNumber(Number) --> [Number], {string(Number)}.
-class --> classWord, classNumber(Number), classNumber(NumberTwo), conjonction, classNumber(NumberThird).
-class --> classWord, classNumber(Number), conjonction, classNumber(NumberBis).
-class --> classWord, classNumber(Number).
+class(ClassList) --> classWord, classNumber(ClassOne), classNumber(ClassTwo), conjonction, classNumber(ClassThree).
+class([ClassOne, ClassTwo]) --> classWord, classNumber(ClassOne), conjonction, classNumber(ClassTwo).
+class([Number]) --> classWord, classNumber(Number).
 %TODO: add several classes in a better way
 
 % Room
@@ -43,14 +43,29 @@ classDescription --> ["is", "after"].
 classDescription --> ["are", "on", "the", "same", "day"].
 roomDescription --> ["seats"].
 conjonction --> ["and"].
-conjonction --> [].
+
 
 % Sentence description
-teacherSentence --> teacher, teacherDescription, class.
-classSentence --> class, classDescription, room.
-classSentence --> class, classDescription.
-classSentence --> class, classDescription, class.
+teacherSentence(StackIn, StackOut) --> teacher(Name), teacherDescription, class(ClassList), { addToStack(StackIn, Name, ClassList, StackOut) }.
+classSentence(StackIn, StackOut) --> class(ClassList), classDescription, room.
+classSentence(StackIn, StackOut) --> class(ClassList), classDescription.
+classSentence(StackIn, StackOut) --> class(ClassList), classDescription, class(ClassList2).
 roomSentence --> room, roomDescription, students.
+
+% Full Sentence
+%testSentence(StackIn, StackOut) --> 
+
+%% emptyStack(X)
+%
+% emptyStack/1 Creates a new empty stack
+%
+% @param X The new stack
+emptyStack(X) :- X = [].
+
+%% addToStack()
+addToStack(Stack, Subject, Var, StackOut) :-
+	StackOut = [(Subject, Var)|Stack].
+
 
 
 % --
@@ -59,19 +74,25 @@ roomSentence --> room, roomDescription, students.
 :- begin_tests(base).
 
 test(prof) :-
-	phrase(teacher, ["prof", "smith"]),
-	phrase(teacher, ["he", "also"]).
+	phrase(teacher("smith"), ["prof", "smith"]),
+	phrase(teacher(_), ["he", "also"]).
 
 test(teaching) :-
-	phrase(teacherSentence, ["prof", "rob", "teaches", "class", "c1"]),
-	phrase(teacherSentence, ["he", "also", "teaches", "class", "c2"]).
+	phrase(teacherSentence(_,_), ["prof", "rob", "teaches", "class", "c1"]),
+	phrase(teacherSentence(_,_), ["he", "also", "teaches", "class", "c2"]).
 
 test(class) :-
-	phrase(classSentence, ["class", "c1", "is", "in", "room", 102]),
-	phrase(classSentence, ["class", "c1", "is", "before", "class", "c2"]),
-	phrase(classSentence, ["classes", "c1", "and", "c2", "are", "in", "the", "same", "room"]),
-	phrase(classSentence, ["classes", "c1", "c2", "and", "c3", "have", "the", "same", "teacher"]).
+	phrase(classSentence(_,_), ["class", "c1", "is", "in", "room", 102]),
+	phrase(classSentence(_,_), ["class", "c1", "is", "before", "class", "c2"]),
+	phrase(classSentence(_,_), ["classes", "c1", "and", "c2", "are", "in", "the", "same", "room"]),
+	phrase(classSentence(_,_), ["classes", "c1", "c2", "and", "c3", "have", "the", "same", "teacher"]).
 
 test(room) :-
 	phrase(roomSentence, ["room", 102, "seats", 100, "students"]).
+
+test(stackTeacher) :-
+	emptyStack(Stack),
+	phrase(teacherSentence(Stack,[("rob",["c1"])]), ["prof", "rob", "teaches", "class", "c1"]),
+	phrase(teacherSentence(Stack,[("he",["c1", "c2"])]), ["he", "also", "teaches", "classes", "c1", "and", "c2"]).
+
 
