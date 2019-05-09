3a6e6abaaa97aee55f5a006715bb617344028985
Myxfall <maximilienromain96@hotmail.com>
2019-05-09 16:07:17 +0200

    [add] push Room goal to stack

--- project.pl
+++ project.pl
@@ -3,22 +3,25 @@
 :- use_module(library(clpfd)).
 
 %% ------ TODO LIST -----
-% 1) 	: generate goal --> teacher(jones, [a1,b2,c3])
+% X) 	: generate goal --> teacher(jones, [a1,b2,c3])
 % 1.1) 	: find way to find fitting goal -> teacher(...) class(...)
 % 2) 	: find way to check "he also" after prof X 
 % -----
 
 
-%% ------ DRAFT IDEAS -----
-% teacher(prof, [courses])
-% classIn([classes], room)
-% teacherBy([classes], prof)
-% populate(class, nbr_students)
-% seats(room, nbr_students)
-% before(class1, class2)
-% after(class1, class2)
-% sameDay([classes])
+%% ------ QUESTIONS LIST ------
+% 1) Split class sentence in order to push to stack the right goal ?
+% -> 
 
+%% ------ DRAFT IDEAS -----
+% teach : teacher(prof, [courses])
+% class : classIn([classes], room)
+% class : teacherBy([classes], prof)
+% class : populate(class, nbr_students)
+% class : before(class1, class2)
+% class : after(class1, class2)
+% class : sameDay([classes])
+% room 	: seats(room, nbr_students)
 
 % --
 % -- MAIN PROGRAM
@@ -37,7 +40,7 @@ classWord --> [class].
 classWord --> [classes].
 %classNumber(Number) --> [Number], {string(Number)}.
 classNumber(Number) --> [Number].
-class(ClassList) --> classWord, classNumber(ClassOne), classNumber(ClassTwo), conjonction, classNumber(ClassThree).
+class([ClassOne, ClassTwo, ClassThree]) --> classWord, classNumber(ClassOne), classNumber(ClassTwo), conjonction, classNumber(ClassThree).
 class([ClassOne, ClassTwo]) --> classWord, classNumber(ClassOne), conjonction, classNumber(ClassTwo).
 class([Number]) --> classWord, classNumber(Number).
 %TODO: add several classes in a better way
@@ -45,12 +48,12 @@ class([Number]) --> classWord, classNumber(Number).
 % Room
 roomWord --> [room].
 roomNumber(Number) --> [Number], {integer(Number)}.
-room --> roomWord, roomNumber(Number).
+room(RoomNumber) --> roomWord, roomNumber(RoomNumber).
 
 % Students
 studentsWord --> [students].
 numberStudents(Number) --> [Number], {integer(Number)}.
-students --> numberStudents(Number), studentsWord.
+students(Number) --> numberStudents(Number), studentsWord.
 
 % Subjects actions
 teacherDescription --> [teaches].
@@ -67,10 +70,10 @@ conjonction --> [and].
 
 % Sentence description
 teacherSentence(StackIn, StackOut) --> teacher(Name), teacherDescription, class(ClassList), { addTeacherToStack(StackIn, Name, ClassList, StackOut) }.
-classSentence(StackIn, StackOut) --> class(ClassList), classDescription, room.
+classSentence(StackIn, StackOut) --> class(ClassList), classDescription, room(RoomNumber).
 classSentence(StackIn, StackOut) --> class(ClassList), classDescription.
 classSentence(StackIn, StackOut) --> class(ClassList), classDescription, class(ClassList2).
-roomSentence --> room, roomDescription, students.
+roomSentence(StackIn, StackOut) --> room(RoomNumber), roomDescription, students(StudentsNumber), { addRoomToStack(StackIn, RoomNumber, StudentsNumber, StackOut) }.
 
 % Full Sentence
 %testSentence(StackIn, StackOut) --> 
@@ -88,7 +91,7 @@ addToStack(Stack, Subject, Var, StackOut) :-
 
 %% addTeacherToStack(Stack, Subject, Var Stackout)
 %
-% addTeacherToStack/4 Add teacher gool to the stack
+% addTeacherToStack/4 Add teacher goal to the stack
 %
 % @param Stack Current Stack
 % @parem Subject Subject of the parsed sentence
@@ -97,6 +100,13 @@ addToStack(Stack, Subject, Var, StackOut) :-
 %
 addTeacherToStack(Stack, Subject, Var, StackOut) :-
 	StackOut = [(teacher(Subject, Var))|Stack].
+
+%% addRoomToStack
+%
+% addRoomToStack Add room goal seating student to the Stack
+%
+addRoomToStack(Stack, RoomName, Seats, [(seats(RoomName, Seats))|Stack]).
+
 
 % --
 % -- TEST
@@ -118,14 +128,16 @@ test(class) :-
 	phrase(classSentence(_,_), [classes, c1, c2, and, c3, have, the, same, teacher]).
 
 test(room) :-
-	phrase(roomSentence, [room, 102, seats, 100, students]).
+	phrase(roomSentence(_,_), [room, 102, seats, 100, students]).
 
 test(stackTeacher) :- 
 	emptyStack(Stack),
 	phrase(teacherSentence(Stack,[teacher(rob, [c1])]), [prof, rob, teaches, class, c1]),	
 	phrase(teacherSentence(Stack,[teacher(steve, [c1,c2])]), [prof, steve, teaches, classes, c1, and, c2]).	
 
-
+test(stackRoom) :-
+	emptyStack(Stack),
+	phrase(roomSentence(Stack, [seats(102, 100)]), [room, 102, seats, 100, students]).
 
 
 
